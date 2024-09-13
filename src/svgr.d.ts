fc4876bb2a36dbc644527dd6db10e90e16ae53d0
Noah Moore <wobblybloom92@gmail.com>
2024-09-13 03:28:39 +0300

    transitioned to vite

--- src/svgr.d.ts
+++
@@ -1,9 +0,0 @@
-// / <reference types="vite-plugin-svgr/client" />
-
-declare module '*.svg' {
-    import { ReactElement, SVGProps } from 'react';
-    const ReactComponent: (props: SVGProps<SVGSVGElement>) => ReactElement;
-    export { ReactComponent };
-    export default ReactComponent;
-  }
-  
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  test: {
+    environment: 'jsdom', // Ensures tests run in a jsdom environment
+    globals: true,
+    setupFiles: ['src/setupTests.ts']
+  },
 })
